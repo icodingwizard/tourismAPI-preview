960ab7a42a8898c1d88ce0f962bb23237145214c
icodingwizard <abdullahalshhab@proton.me>
2023-12-10 02:56:51 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from 'react'
 
-import './styles/App.css';
+// import './styles/App.css';
+import Card from './components/Card';
 
 function App() {
   const [image, setImage] = useState(null);
 
-  useEffect(()=>{
-    fetch('https://libyantourismapi.vercel.app/places')
-    .then(res => res.json())
-    .then(data => setImage(data[0].Image));
-  },[]);
+  // useEffect(() => {
+  //   fetch('https://libyantourismapi.vercel.app/places')
+  //   .then(res => res.json())
+  //   .then(data => console.log(data));
+  // },[]);
 
 
 
   return (
-    <div>
-        
-    </div>
+    <>
+          <Card />  
+    </>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,23 @@
-function Card (){
-    return <></>;
+import { useEffect, useRef } from 'react';
+import styles from '../styles/Card.module.css'
+
+function Card (props){
+
+    const cardRef = useRef();
+
+    useEffect(()=>{});
+
+
+
+    return( 
+        <div className = {styles.Card} ref={cardRef}>
+            
+            {/* <img src={props.image} alt="" />
+            <p className="title">{props.title}</p>
+            <p className="description">{props.description}</p> */}
+saddscds
+        </div>
+    );
 }
 
 export default Card;
